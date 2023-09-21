344e32e817c2288177cc0644f8bedde509c874f9
Harshithagopidi <harshithareddygopidi3010@gmail.com>
2023-09-21 11:43:34 -0700

    again same repo

---
+++ DoLoop.java
@@ -0,0 +1,16 @@
+package Pratice;
+
+public class DoLoop {
+
+	public static void main(String[] args) {
+		
+		int a= 1;
+		do {
+		 System.out.println(a);
+		 a++;
+	}
+	    while(a<5);
+		
+	}
+
+}
